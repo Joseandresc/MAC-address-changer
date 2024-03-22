b7023d46460d4eb9c7555a18172d442c5d88cc89
Jose <joseandresc@microsoft.com>
2024-03-22 14:31:06 -0400

    Modified script

--- MAC-Address-changer/main.py
+++ MAC-Address-changer/main.py
@@ -1,12 +1,34 @@
 
 import subprocess
 import optparse
+import re
 
-parser = optparse.OptionParser()
+def get_arguments():
+    # parser object is created here
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        #handle error
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a MAC, use --help for more info")
+    return options
+def change_mac(interface, new_mac):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-interface = input("interface >")
-new_mac = input("MAC >")
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    regexMac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+    if regexMac:
+        print(regexMac.group(0))
+    else:
+        print("Could not find MAC address")
+
+options = get_arguments()
+# change_mac(options.interface,options.new_mac)
+
